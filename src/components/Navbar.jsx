8ecde0920c9af2255ac8cbc4b8a66bc9a6b533ac
siva946 <123248824+siva946@users.noreply.github.com>
2025-08-24 19:51:52 +0530

    Navbar responsive changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import {FaSearch,FaBell,FaUser} from "react-icons/fa";
+import {FaTimes,FaBars,FaSearch,FaBell,FaUser} from "react-icons/fa";
 import { useState } from "react";
 
 function Navbar() {
@@ -10,21 +10,30 @@ function Navbar() {
     { name: "My List", link: "/Mylist" },
   ];
   const [isNavLinkHovered,setIsnavlinkhovered]=useState(null);
-
+  const[isOpened,setIsopened]=useState(false);
+const toggleSidebar=()=>{
+  setIsopened(!isOpened);
+}
   return (
-    <header className="justify-content-between align-items-center bg-dark text-center pt-2 pe-4 px-4" 
-    style={{border:"0.5px solid #191e25"}}>
-      <p style={{color:"white",fontSize:"1.5rem",fontWeight:"bold"}}>ZuuuFlix</p>
-      <div className="navbar">
+    <header
+      className="d-flex justify-content-between align-items-center bg-dark text-center pt-2 px-4"
+    >
+      <p style={{ color: "white", fontSize: "1.5rem", fontWeight: "bold" }}>
+        ZuuuFlix
+      </p>
+      <div className={`sidebar ${isOpened?'open':'closed'} `}>
         <ul className="navbar-list d-flex justify-content-center gap-4">
           {navbarItems.map((item, index) => (
-            <li key={index} style={{listStyleType:"none"}}>
+            <li key={index} style={{ listStyleType: "none" }}>
               <Link
                 to={item.link}
-                style={{ textDecoration: "none", color:isNavLinkHovered===index?"white":"gray",transition:"all 0.3s ease"
+                style={{
+                  textDecoration: "none",
+                  color: isNavLinkHovered === index ? "white" : "gray",
+                  transition: "all 0.3s ease",
                 }}
-                onMouseEnter={()=>setIsnavlinkhovered(index)}
-                onMouseLeave={()=>setIsnavlinkhovered(null)}
+                onMouseEnter={() => setIsnavlinkhovered(index)}
+                onMouseLeave={() => setIsnavlinkhovered(null)}
               >
                 {item.name}
               </Link>
@@ -32,13 +41,21 @@ function Navbar() {
           ))}
         </ul>
       </div>
-      <div className="other-links text-white" >
+      <div className="other-links text-white">
         <ul className="link-items d-flex justify-content-center gap-4">
-          <li><FaSearch/></li>
-          <li><FaBell/></li>
-          <li><FaUser/></li>
+          <li>
+            <FaSearch />
+          </li>
+          <li>
+            <FaBell />
+          </li>
+          <li>
+            <FaUser />
+          </li>
         </ul>
       </div>
+      <button className="toggle-button"
+      onClick={toggleSidebar}>{isOpened?<FaTimes/>:<FaBars/>}</button>
     </header>
   );
 }
